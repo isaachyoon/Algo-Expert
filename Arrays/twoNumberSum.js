5674eb23ba4a6a0da36cf9368af8376f8d9365d2
Isaac Yoon <ysaacioon@yahoo.com>
2019-04-27 13:32:21 -0700

    twoNumberSum

--- Arrays/twoNumberSum.js
+++ Arrays/twoNumberSum.js
@@ -1,18 +1,32 @@
 //easy
+// function twoNumberSum(array, targetSum) {
+//   array = array.sort((a, b) => a - b);
+//   let p1 = 0,
+//       p2 = array.length - 1;
+//   while (p1 < p2) {
+//     let sum = array[p1] + array[p2];
+//     if (sum === targetSum) {
+//       return [array[p1], array[p2]];
+//     } else if (sum < targetSum){
+//       p1++;
+//     } else {
+//       p2--;
+//     }
+//   }
+//   return [];
+// }
+
+//version two using additional space
+
 function twoNumberSum(array, targetSum) {
-  array = array.sort((a, b) => a - b);
-  let p1 = 0,
-      p2 = array.length - 1;
-  while (p1 < p2) {
-    let sum = array[p1] + array[p2];
-    if (sum === targetSum) {
-      return [array[p1], array[p2]];
-    } else if (sum < targetSum){
-      p1++;
-    } else {
-      p2--;
+  let set = new Set();
+  for (let num of array) {
+    const searchFor = targetSum - num;
+    if (set.has(searchFor)) {
+      return [num, searchFor]
     }
-  }
+    set.add(num);
+  };
   return [];
 }
 
