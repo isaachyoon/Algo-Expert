2ce7d1b3f7685c76d53b404b884e42cd856213b4
Isaac Yoon <ysaacioon@yahoo.com>
2019-05-18 17:48:30 -0700

    min number of jumps

--- Dynamic Programming/minNumberOfJumps.js
+++ Dynamic Programming/minNumberOfJumps.js
@@ -35,5 +35,25 @@ function minNumberOfJumps(array) {
   return res[res.length -1];
 }
 
-let res = minNumberOfJumps([3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3]);
+//always keep track of the maximum and reduce the max by one, everytime it's moved over.
+function minNumberOfJumps(array) {
+  array[array.length - 1] = 0;
+  for (let i = array.length - 2; i >= 0; i--) {
+    let numSteps = array[i];
+    let min = Infinity;
+    for (let j = i + 1; j <= Math.min(i + numSteps, array.length -1); j++) {
+      min = Math.min(min, array[j]);
+    }
+    array[i] = min + 1;
+  } 
+  return array[0];
+}
+
+//1st appraoch  (n x m x o) complexity ==> you can map out all possible outcome and recursively find the minimum number of steps
+//2nd approach  use dynamic programing to see how many steps were taken so far (constant space + n^2 time complexity)
+
+let input1 = [3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3];
+let input2 = [1, 1];
+let input3 = [2, 1, 2, 3, 1, 1, 1];
+let res = minNumberOfJumps(input3);
 console.log(res);
